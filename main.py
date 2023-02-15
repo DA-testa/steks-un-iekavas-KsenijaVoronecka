1398ab610259947caa90fdadea8197ddb10c2871
KsenijaVoronecka <124842373+KsenijaVoronecka@users.noreply.github.com>
2023-02-15 17:17:25 +0000

    update main file

--- main.py
+++ main.py
@@ -14,17 +14,45 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i+1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[-1].char, next) == False: 
+                return i+1
+            
+            opening_brackets_stack.pop()
+    
+    if len(opening_brackets_stack) == 0:
+        return 0
+    else:
+        return opening_brackets_stack[-1].position
+
 
 
 def main():
-    text = input()
+    command = input()
+
+    if "I" in command:
+        text = input()
+    elif "F" in command:
+        print("Ievadiet testa faila nosaukumu: ")
+        fileNumber = input()
+        file1 = "txt"
+        file2 = "."
+        fileName = "\\test\\" + fileNumber + file2 + file1
+        file = open(fileName, "r")
+        text = file.read()
+    else:
+        print("Error")
+        return
+
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if mismatch == 0:
+        print("Success")
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
